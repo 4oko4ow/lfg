75c0a1f3989deb1730530a61f5a7ef2a61cbe93d
4oko4ow <cerberussoftware823@gmail.com>
2025-08-01 12:38:45 +0300

    up

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'react-hot-toast'
+import { BrowserRouter } from 'react-router-dom'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-     <Toaster position="top-right" />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <Toaster position="top-right" />
   </StrictMode>,
 )
